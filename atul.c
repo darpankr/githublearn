d2ee2a913e4681bd5b66d63c10c20d5a14294f6c
darpankr <54790420+darpankr@users.noreply.github.com>
2020-05-13 21:51:52 +0530

    second commit

---
+++ atul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,n;
+    printf("enter the value of n\n");
+    scanf("%d",&n);
+    i=1;
+    do
+    {
+        printf("\n welcome\n");
+        i++;
+    }
+    while(i<=n);
+    printf("\nGood bye\n");
+
+    return 0;
+}
